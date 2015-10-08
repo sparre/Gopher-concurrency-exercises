a97e9f830d7ef52b4c59d50b5125ba27e408d29a
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-10-08 10:09:49 +0200

    Basic door and alarm interaction.

--- src/daily_walk.adb
+++ src/daily_walk.adb
@@ -8,6 +8,52 @@ procedure Daily_Walk is
       entry Put_Line (Item : in String);
    end Output;
 
+   protected Door is
+      procedure Unlock;
+      procedure Lock;
+      function Is_Locked return Boolean;
+   private
+      Locked : Boolean := False;
+   end Door;
+
+   task Alarm is
+      entry Arm;
+   end Alarm;
+
+   task body Alarm is
+      Armed : Boolean := False;
+   begin
+      loop
+         select
+            accept Arm;
+            delay 60.0;
+            Armed := True;
+         or
+            delay 0.1;
+            if Armed and not Door.Is_Locked then
+               Output.Put_Line ("Alarm ringing.");
+            end if;
+         end select;
+      end loop;
+   end Alarm;
+
+   protected body Door is
+      function Is_Locked return Boolean is
+      begin
+         return Locked;
+      end Is_Locked;
+
+      procedure Lock is
+      begin
+         Locked := True;
+      end Lock;
+
+      procedure Unlock is
+      begin
+         Locked := False;
+      end Unlock;
+   end Door;
+
    task body Output is
       use Ada.Exceptions, Ada.Strings.Unbounded, Ada.Text_IO;
       Buffer : Unbounded_String;
@@ -30,5 +76,13 @@ procedure Daily_Walk is
                            Exception_Message (E));
    end Output;
 begin
-   Output.Put_Line ("Hello world.");
+   Door.Unlock;
+   Output.Put_Line ("Arming alarm...");
+   Alarm.Arm;
+   Output.Put_Line ("Waiting 30 seconds...");
+   delay 30.0;
+   Output.Put_Line ("Locking door...");
+   Door.Lock;
+
+   abort Alarm;
 end Daily_Walk;
